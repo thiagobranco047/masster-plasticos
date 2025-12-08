25673f02aff1f8840632084554a223c91b8f2118
thiagobranco047 <thiagobranco.047@gmail.com>
2025-12-08 16:43:16 -0300

    feat: build fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function Home() {
   const {
-    heroRef,
+    heroRef, 
     statsRef,
     featuresRef,
     categoriasRef,
